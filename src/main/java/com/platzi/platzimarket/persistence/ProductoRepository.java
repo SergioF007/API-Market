830aba5cc766137ccc91ac91fc980ac28cb47006
SergioFS_Ing07 <sefernandez0711@gmail.com>
2023-05-09 19:08:36 -0500

    Creamos el paquete mapper y donde implemetamos el mapeo de las entidades entre Categoria y Category -  Producto y Product

--- src/main/java/com/platzi/platzimarket/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/platzimarket/persistence/ProductoRepository.java
@@ -1,7 +1,10 @@
 package com.platzi.platzimarket.persistence;
 
+import com.platzi.platzimarket.domain.Product;
+import com.platzi.platzimarket.domain.repository.ProductRepository;
 import com.platzi.platzimarket.persistence.crud.ProductoCrudRepository;
 import com.platzi.platzimarket.persistence.entity.Producto;
+import com.platzi.platzimarket.persistence.mapper.ProductMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,14 +13,37 @@ import java.util.Optional;
 // con la notacion @Repository le estamos indicando a spring
 // que esta clase se encarga de intereactuar con la BD
 @Repository
-public class ProductoRepository {
+public class ProductoRepository implements ProductRepository {
 
     private ProductoCrudRepository productoCrudRepository;
 
+    private ProductMapper mapper;
+
     // Creamo el metodo para recuperar todos los productos que hay en BD
-    public List<Producto> getAll() {
-        return (List<Producto>) productoCrudRepository.findAll(); // inciialmente nos genera error
+    public List<Product> getAll() {
+        List<Producto> productos = (List<Producto>) productoCrudRepository.findAll(); // inciialmente nos genera error)
+        return mapper.toProcusts(productos);
         // pero nos recomiendan que castiemos((List<Producto>)) el return y se arregla el error.
+    }
+
+    @Override
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<List<Product>> getScarseProducts(int quantity) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Product> getProduct(int productId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Product save(Product product) {
+        return null;
     }
 
     //productos por categoria
