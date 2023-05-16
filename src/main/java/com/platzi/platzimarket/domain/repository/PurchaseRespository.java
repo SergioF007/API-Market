ff6a29c5e9772fcff478cfb3fbd0755268964e2f
SergioFS_Ing07 <sefernandez0711@gmail.com>
2023-05-16 10:58:55 -0500

    Creamos la interfaz PurchaseRepository que va implemetar los metodos que se podran implementar al momento de crear una compra

---
+++ src/main/java/com/platzi/platzimarket/domain/repository/PurchaseRespository.java
@@ -0,0 +1,23 @@
+package com.platzi.platzimarket.domain.repository;
+
+import com.platzi.platzimarket.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRespository {
+
+    // Creamos 3 metodos a implementar
+
+    // todas la compras
+    List<Purchase> getAll();
+
+    // compras por cliente
+    Optional<List<Purchase>> getByClient(String clientId);
+
+    // guardar compra
+    Purchase save(Purchase purchase);
+
+
+
+}
